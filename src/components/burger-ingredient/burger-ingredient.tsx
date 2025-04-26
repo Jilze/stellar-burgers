6159d934156ca395fa4b766a307f52543ba2b7b1
Matvey M <129954763+Jilze@users.noreply.github.com>
2025-04-26 15:44:07 +0300

    Revert "stellar-burgers v1.0"

--- src/components/burger-ingredient/burger-ingredient.tsx
+++ src/components/burger-ingredient/burger-ingredient.tsx
@@ -1,21 +1,21 @@
 import { FC, memo } from 'react';
 import { useLocation } from 'react-router-dom';
+
 import { BurgerIngredientUI } from '@ui';
 import { TBurgerIngredientProps } from './type';
-import { useDispatch } from '../../services/store';
-import { addItem } from '../../slices/constructor';
 
 export const BurgerIngredient: FC<TBurgerIngredientProps> = memo(
   ({ ingredient, count }) => {
-    const dispatch = useDispatch();
+    const location = useLocation();
+
+    const handleAdd = () => {};
+
     return (
       <BurgerIngredientUI
         ingredient={ingredient}
         count={count}
-        locationState={{ background: useLocation() }}
-        handleAdd={() =>
-          dispatch(addItem({ ...ingredient, id: ingredient._id }))
-        }
+        locationState={{ background: location }}
+        handleAdd={handleAdd}
       />
     );
   }
--- src/components/profile-menu/profile-menu.tsx
+++ src/components/profile-menu/profile-menu.tsx
@@ -1,15 +1,11 @@
 import { FC } from 'react';
 import { useLocation } from 'react-router-dom';
 import { ProfileMenuUI } from '@ui';
-import { useDispatch } from '../../services/store';
-import { logout } from '../../slices/user';
 
 export const ProfileMenu: FC = () => {
   const { pathname } = useLocation();
-  return (
-    <ProfileMenuUI
-      handleLogout={() => useDispatch()(logout())}
-      pathname={pathname}
-    />
-  );
+
+  const handleLogout = () => {};
+
+  return <ProfileMenuUI handleLogout={handleLogout} pathname={pathname} />;
 };
